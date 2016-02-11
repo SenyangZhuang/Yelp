bc11a4975be8795b67632ece76c3165f569afcf7
Senyang Zhuang <senyangzhuang@163.com>
2016-02-11 13:51:57 -0600

    "Filter Function Implemented"

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,43 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Senyang Zhuang on 2/10/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol SwitchCellDelegate{
+    optional func switchCell(switchCell: SwitchCell , didChangeValue value: Bool)
+    
+    
+}
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var onSwitch: UISwitch!
+    @IBOutlet weak var switchLabel: UILabel!
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        onSwitch.addTarget(self, action: "switchValueChanged", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func switchValueChanged(){
+        //print("WWWWWW")
+        delegate?.switchCell?(self, didChangeValue: onSwitch.on)
+    }
+    
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
